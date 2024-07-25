9a0467b4abe7350192192db42f339e0f54f47e30
Jonas Senft-Raiss <j.senft@campus.lmu.de>
2024-07-25 13:57:02 +0200

    Add unit tests for reward model

---
+++ prefacc/training/reward_model/test_reward_model.py
@@ -0,0 +1,20 @@
+"""Reward model tests."""
+
+from absl.testing import absltest
+from prefacc.training.reward_model import reward_model
+
+
+class RewardModelTest(absltest.TestCase):
+  """Tests for reward model module."""
+
+  def testRewardModelCreation(self):
+    """Test reward model creation."""
+    obs_size = 3
+    action_size = 2
+    # model = reward_model.make_reward_network(obs_size, action_size)
+    model = reward_model.make_reward_network(obs_size, action_size)
+    self.assertIsNotNone(model)
+
+
+if __name__ == '__main__':
+  absltest.main()
